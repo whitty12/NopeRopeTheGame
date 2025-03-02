6f9b09917e73a0cd1247dc885b08c426b93dbbbc
Stephanie Whitworth <stephanierwhitworth12@gmail.com>
2025-03-01 19:19:44 -0700

    Code does not run; this is the start of adding segments when the snake eats a piece of food.

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
 #Import the pygame library
 import pygame
 from pygame import RESIZABLE
+import numpy as np
 
 
 #constant variables
 SNEKCOLOR = (0,255,0) #Green
 FOODCOLOR = (255,222,33) #Yellow
+NOPEROPECOLOR = (255,228,196) #variation of brown, for the body segments
 BLACK = (0,0,0,) #Black
 WIDTH_OF_WINDOW = 600
 HEIGHT_OF_WINDOW = 600
-SNEK_HEAD_SIZE = (20,20,20,20)
+SNEK_HEAD = (20,20,20,20) #this is the x,y coordinates as well as the box dimensions
+SNEK_SEGMENT_SIZE = (20,20) #this is the dimensions of each box
 
 #Global variables
 pos_x = 0
 pos_y = 0
 food_size = (220, 300, 20,20)
+num_of_nope_ropes = 0;
 
 #Create a resizable window of size determined in the setting variables using .set_mode function
 window = pygame.display.set_mode((WIDTH_OF_WINDOW,HEIGHT_OF_WINDOW))
-snek_head = pygame.Rect(SNEK_HEAD_SIZE)
+snek_head = pygame.Rect(SNEK_HEAD)
+snek_segment = pygame.Rect(SNEK_SEGMENT_SIZE) #create a segment that will be drawn below the head when food is caught
 clock = pygame.time.Clock()
 food = pygame.Rect(food_size)
+
 ############################################
 def foodCaught(head_xpos, head_ypos, food_xpos, food_ypos):
     if (head_xpos == food_xpos) and (head_ypos == food_ypos):
@@ -64,6 +70,16 @@ while running == True:
     #if food is caught we need to delete and respawn elsewhere
     if foodCaught(snek_head.x, snek_head.y, food.x, food.y):
         pygame.draw.rect(window, SNEKCOLOR, food)
+        num_of_nope_ropes += 1 #increment segments to be painted by 1
+
+        if num_of_nope_ropes > 1:
+            for segment in num_of_nope_ropes:
+                snek_segment.x = snek_segment.x - 20
+                snek_segment.y = snek_segment.y - 20
+                pygame.draw.rect(window, NOPEROPECOLOR, snek_segment)
+        else:
+            pygame.draw.rect(window, NOPEROPECOLOR, snek_segment)
+
         food_spawned = False
 
     #update the state of the screen
