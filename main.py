b23248494ebbbc17cb301ab1588ec5b4845efa9f
Stephanie Whitworth <stephanierwhitworth12@gmail.com>
2025-03-10 13:58:00 -0600

    Changed the way the snake body is implement so it now draws an array of positions; however, current implementation pops the ENTIRE array and no longer lines up correctly with the pieces of food.

--- main.py
+++ main.py
@@ -11,19 +11,21 @@ NOPEROPECOLOR = (255,228,196) #variation of brown, for the body segments
 BLACK = (0,0,0,) #Black
 WIDTH_OF_WINDOW = 600
 HEIGHT_OF_WINDOW = 600
-SNEK_HEAD = (20,20,20,20) #this is the x,y coordinates as well as the box dimensions
 SNEK_SEGMENT_SIZE = (20,20) #this is the dimensions of each box
 
 #Global variables
-pos_x = 0
-pos_y = 0
-food_size = (220, 300, 20,20)
-num_of_nope_ropes = 0;
+#default food dimensions
+food_size = [220, 300, 20,20]
+#replace individual x, y value with one array
+snek_head_position = [20,50]
+#snake default (TODO: Randomize starting position later)
+snek_nope_rope = [[20,50],
+                  [20,40],
+                  [20,30],
+                  [20,20]]
 
 #Create a resizable window of size determined in the setting variables using .set_mode function
 window = pygame.display.set_mode((WIDTH_OF_WINDOW,HEIGHT_OF_WINDOW))
-snek_head = pygame.Rect(SNEK_HEAD)
-snek_segment = pygame.Rect(SNEK_SEGMENT_SIZE) #create a segment that will be drawn below the head when food is caught
 clock = pygame.time.Clock()
 food = pygame.Rect(food_size)
 
@@ -49,38 +51,44 @@ while running == True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN:
+            #NOTE: swap from move_ip to simple incrementing as it is not a rectangle and therefore does not use this method
             if event.key == pygame.K_LEFT:
-                snek_head.move_ip(-20,0)
+                snek_head_position[0] -= 20
             elif event.key == pygame.K_RIGHT:
-                snek_head.move_ip(20,0)
+                snek_head_position[0] += 20
             elif event.key == pygame.K_DOWN:
-                snek_head.move_ip(0,20)
+                snek_head_position[1] += 20
             elif event.key == pygame.K_UP:
-                snek_head.move_ip(0,-20)
+                snek_head_position[1] -= 20
 
+    #we fill BEFORE to prevent overwriting as well as to erase the previous movement drawn on the screen
     window.fill(BLACK)
-    #snake head
-    pygame.draw.rect(window, SNEKCOLOR, snek_head)
+
+    snek_nope_rope.insert(0,list(snek_head_position))
+
+    #initial snake body and draw the array
+    for segment in snek_nope_rope:
+        pygame.draw.rect(window, SNEKCOLOR, pygame.Rect(segment[0],segment[1], 20,20))
 
     #check if the food is spawned and draw our next food piece
     if food_spawned == True:
         pygame.draw.rect(window, FOODCOLOR, food)
 
     #if food is caught we need to delete and respawn elsewhere
-    if foodCaught(snek_head.x, snek_head.y, food.x, food.y):
+    #TODO: Fix the comparison method to ensure the x/y can always align to trigger this condition
+    if foodCaught(snek_head_position[0], snek_head_position[1], food.x, food.y):
         pygame.draw.rect(window, SNEKCOLOR, food)
-        num_of_nope_ropes += 1 #increment segments to be painted by 1
-
-        if num_of_nope_ropes > 1:
-            for segment in num_of_nope_ropes:
-                snek_segment.x = snek_segment.x - 20
-                snek_segment.y = snek_segment.y - 20
-                pygame.draw.rect(window, NOPEROPECOLOR, snek_segment)
-        else:
-            pygame.draw.rect(window, NOPEROPECOLOR, snek_segment)
-
+        #increment number of body parts
+        # insert at the start of our snek array
         food_spawned = False
+        #for segment in num_of_nope_ropes:
+        #    snek_segment.x = snek_segment.x - 20
+        #    snek_segment.y = snek_segment.y - 20
+        #    pygame.draw.rect(window, NOPEROPECOLOR, snek_segment)
+
+    #TODO: Fix the removal of every element from the snake body as we only want to remove part of it
+    #snek_nope_rope.pop()
 
     #update the state of the screen
     pygame.display.update()
